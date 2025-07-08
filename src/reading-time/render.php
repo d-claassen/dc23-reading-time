9d30f8c9cb14e25fb2bfd90d9cd91b93ea7d43d3
Dennis Claassen <declaassen@gmail.com>
2025-07-08 23:56:54 +0200

    Update render.php

--- src/reading-time/render.php
+++ src/reading-time/render.php
@@ -5,7 +5,7 @@
 ?>
 <p <?php echo get_block_wrapper_attributes(); ?>>
 	<?php 
-    $minutes = 11;
+    $minutes = YoastSEO()->meta->for_current_page()->estimated_reading_time_minutes;
     esc_html_e(
         sprintf(
             _n( 'Estimated reading time: %n minute', 'Estimated reading time: %n minutes', $minutes, 'reading-time' ),
