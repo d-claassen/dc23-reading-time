683566e458c36066a824cee167fe42280126ce00
Dennis Claassen <declaassen@gmail.com>
2025-07-08 23:46:39 +0200

    Update edit.js

--- src/reading-time/edit.js
+++ src/reading-time/edit.js
@@ -1,16 +1,5 @@
-/**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
- */
+import { useSelect } from '@wordpress/data';
 import { _n, sprintf } from '@wordpress/i18n';
-
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
 import { useBlockProps } from '@wordpress/block-editor';
 
 /**
