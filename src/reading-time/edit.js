e0bbb58e54df31d8048708995ac7d82fb7505e1a
Dennis Claassen <declaassen@gmail.com>
2025-07-14 21:54:54 +0200

    Add reading time block (#219)

---
+++ src/reading-time/edit.js
@@ -0,0 +1,36 @@
+import { useSelect } from '@wordpress/data';
+import { _n, sprintf } from '@wordpress/i18n';
+import { useBlockProps } from '@wordpress/block-editor';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './editor.scss';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+ *
+ * @return {Element} Element to render.
+ */
+export default function Edit() {
+    const { minutes } = useSelect( select => {
+        return {
+            minutes: select( "yoast-seo/editor" ).getEstimatedReadingTime(),
+        };
+    }, [] );
+
+    return (
+        <p { ...useBlockProps() }>
+            { sprintf(
+                _n( 'Estimated reading time: %d minute', 'Estimated reading time: %d minutes', minutes, 'reading-time' ),
+                minutes
+            ) }
+        </p>
+    );
+}
