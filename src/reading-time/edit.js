09ee441222ae7a66a2405209fbaf4b9ac9ab9b24
Dennis Claassen <declaassen@gmail.com>
2025-07-26 00:17:03 +0200

    Prefix in edit block

--- src/reading-time/edit.js
+++ src/reading-time/edit.js
@@ -1,38 +1,57 @@
-import { useSelect } from '@wordpress/data';
-import { _n, sprintf } from '@wordpress/i18n';
-import { useBlockProps } from '@wordpress/block-editor';
+import { useSelect } from ‘@wordpress/data’;
+import { _n, sprintf, __ } from ‘@wordpress/i18n’;
+import { useBlockProps, RichText } from ‘@wordpress/block-editor’;
 
 /**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * Those files can contain any CSS code that gets applied to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
-import './editor.scss';
+
+- Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+- Those files can contain any CSS code that gets applied to the editor.
+- 
+- @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+  */
+  import ‘./editor.scss’;
 
 /**
- * The edit function describes the structure of your block in the context of the
- * editor. This represents what the editor will render when the block is used.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
- *
- * @return {Element} Element to render.
- */
-export default function Edit() {
-    const { minutes } = useSelect( select => {
-        const store = select( "yoast-seo/editor" );
 
-        return {
-            minutes: store?.getEstimatedReadingTime() ?? 42,
-        };
-    }, [] );
-
-    return (
-        <p { ...useBlockProps() }>
-            { sprintf(
-                _n( 'Estimated reading time: %d minute', 'Estimated reading time: %d minutes', minutes, 'reading-time' ),
-                minutes
-            ) }
-        </p>
-    );
-}
+- The edit function describes the structure of your block in the context of the
+- editor. This represents what the editor will render when the block is used.
+- 
+- @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+- 
+- @param {Object} props - Block props
+- @param {Object} props.attributes - Block attributes
+- @param {Function} props.setAttributes - Function to update attributes
+- @return {Element} Element to render.
+  */
+  export default function Edit({ attributes, setAttributes }) {
+  const { prefix } = attributes;
+  
+  const { minutes } = useSelect( select => {
+  const store = select( “yoast-seo/editor” );
+  
+  ```
+   return {
+       minutes: store?.getEstimatedReadingTime() ?? 42,
+   };
+  ```
+  
+  }, [] );
+  
+  const minutesText = minutes === 1 ? __(‘minute’, ‘reading-time’) : __(‘minutes’, ‘reading-time’);
+  const defaultPrefix = __(‘Estimated reading time:’, ‘reading-time’);
+  
+  return (
+  <p { …useBlockProps() }>
+  <RichText
+  tagName=“span”
+  value={prefix}
+  onChange={(value) => setAttributes({ prefix: value })}
+  placeholder={defaultPrefix}
+  allowedFormats={[]}
+  className=“reading-time-prefix”
+  />
+  {prefix && ’ ’}
+  {minutes} {minutesText}
+  </p>
+  );
+  }
