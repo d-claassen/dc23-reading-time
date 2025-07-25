9bf3f2cfbf6cbde0c33f2c6c3e7a3fca2308fb84
Dennis Claassen <declaassen@gmail.com>
2025-07-26 01:35:06 +0200

    Update render.php

--- src/reading-time/render.php
+++ src/reading-time/render.php
@@ -10,8 +10,9 @@ $prefix = $attributes['prefix'] ?? '';
 <p <?php echo get_block_wrapper_attributes(); ?>>
 	<?php if ( ! empty( $prefix ) ) : ?>
 		<span class="reading-time-prefix"><?php echo esc_html( $prefix ); ?> </span>
-	<?php endif; ?>
-		<?php echo esc_html(
+	<?php 
+		endif;
+		echo esc_html(
 			sprintf(
 				_n( '%1$s minute', '%1$s minutes', $minutes, 'reading-time' ),
 				$minutes
