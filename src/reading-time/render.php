4ee26b87821a0b04040993f2704f69f6df4471b5
Dennis Claassen <declaassen@gmail.com>
2025-07-26 00:17:58 +0200

    Prefix in dynamic render

--- src/reading-time/render.php
+++ src/reading-time/render.php
@@ -2,15 +2,24 @@
 /**
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
+
+$minutes = YoastSEO()->meta->for_current_page()->estimated_reading_time_minutes;
+$prefix = $attributes['prefix'] ?? '';
 ?>
+
 <p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php 
-    $minutes = YoastSEO()->meta->for_current_page()->estimated_reading_time_minutes;
-    echo esc_html(
-        sprintf(
-            _n( 'Estimated reading time: %d minute', 'Estimated reading time: %d minutes', $minutes, 'reading-time' ),
-            $minutes
-        )
-    );
-    ?>
+	<?php if ( ! empty( $prefix ) ) : ?>
+		<span class="reading-time-prefix"><?php echo esc_html( $prefix ); ?></span>
+		<?php echo ' '; ?>
+		<?php echo esc_html( $minutes ); ?>
+		<?php echo ' '; ?>
+		<?php echo esc_html( $minutes === 1 ? __( 'minute', 'reading-time' ) : __( 'minutes', 'reading-time' ) ); ?>
+	<?php else : ?>
+		<?php echo esc_html(
+			sprintf(
+				_n( 'Estimated reading time: %d minute', 'Estimated reading time: %d minutes', $minutes, 'reading-time' ),
+				$minutes
+			)
+		); ?>
+	<?php endif; ?>
 </p>
