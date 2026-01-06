b92beb1cb76ad2e68a0224e49ddb28d832d5f61f
d-claassen <d-claassen@users.noreply.github.com>
2026-01-06 18:23:45 +0000

    0.3.1

--- dc23-reading-time.php
+++ dc23-reading-time.php
@@ -7,7 +7,7 @@
  *
  * @wordpress-plugin
  * Plugin Name: DC23 Reading Time
- * Version: 0.3.0
+ * Version: 0.3.1
  * Description: Add more features related to reading time in your WordPress website.
  * Requires at least: 6.2
  * Requires PHP: 8.1
